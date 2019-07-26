3a260e554b3263dfe90ae45bd683e1a28fa76f62
katiedodd <ktdodd511@gmail.com>
2019-07-26 16:48:36 -0700

    added links to navbar

--- src/components/music/News.js
+++ src/components/music/News.js
@@ -6,10 +6,10 @@ class News extends Component {
   render() {
     return (
       <div>
-        <h2 className="text-center"> News</h2>
         <div class="container mt-4 services-text">
           <h3 className="text-center"><Link to="/"> Back to Home </Link></h3>
         </div>
+        <h2 className="text-center"> News</h2>
       </div>
     )
   }
